c4c8eca164042e6b91370116ae85488ba3ec4ed0
fbock <fbock@student.42heilbronn.de>
2024-09-03 13:27:01 +0200

    remove duplicate class

--- includes/webserver.hpp
+++ includes/webserver.hpp
@@ -10,16 +10,3 @@ public:
 	~WebServer();
 	void serve();
 };
-
-#pragma once
-
-#include "configuration.hpp"
-#include "http.hpp"
-
-class WebServer
-{
-public:
-	WebServer(WebServerConfig &config);
-	~WebServer();
-	void serve();
-};
