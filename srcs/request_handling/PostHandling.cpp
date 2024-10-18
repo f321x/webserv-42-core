f13bc028085cc7b108de3d16c78f840dd61decc2
nburchha <niklas@burchhardt.de>
2024-10-18 09:00:52 +0000

    not working currently, just pushing to be able continue to work at school

--- srcs/request_handling/PostHandling.cpp
+++ srcs/request_handling/PostHandling.cpp
@@ -1,6 +1,8 @@
 #include "RequestHandler.hpp"
 
-void handle_post(const RequestPacket &request_packet, ResponsePacket &response_packet, const std::pair<ServerConfig, RouteConfig> &config_pair)
+// TODO: check if the old response_packet has final response set -> also set it for the new response_packet
+
+std::unique_ptr<ResponsePacket> handle_post(const RequestPacket &request_packet, std::unique_ptr<ResponsePacket> response_packet, const std::pair<ServerConfig, RouteConfig> &config_pair)
 {
 	DEBUG("Handling POST request");
 	DEBUG("isCgi? " + std::string(1, config_pair.second.isCgi() ? '1' : '0'));
@@ -15,14 +17,15 @@ void handle_post(const RequestPacket &request_packet, ResponsePacket &response_p
 			DEBUG("after execute");
 			std::string cgi_response = cgi.getResponse();
 			DEBUG("CGI response: " + cgi_response);
-			auto tmp = ResponsePacket(cgi_response);
-			response_packet = tmp;
+			bool final_response = response_packet.is_final_response();
+			response_packet = std::make_unique<ResponsePacket>(ResponsePacket(cgi_response));
+			response_packet.set_final_response(final_response);
+			return response_packet;
 		}
 		catch (std::exception &e)
 		{
 			ERROR("CGI error: " + std::string(e.what()));
-			// return internal_server_error();
-			return;
+			return internal_server_error();
 		}
 	}
 	else
@@ -34,4 +37,22 @@ void handle_post(const RequestPacket &request_packet, ResponsePacket &response_p
 		// 	return not_found(load_error_page(404, config_pair.first));
 		// return ok_with_content(file.value(), std::move(response_packet));
 	}
+}
+
+void handleUpload(const RequestPacket &request_packet, ResponsePacket &response_packet, const std::pair<ServerConfig, RouteConfig> &config_pair)
+{
+	DEBUG("Handling upload request");
+	std::string path = config_pair.second.getRoot() + request_packet.getUri();
+	std::ofstream file(path, std::ios::binary);
+	if (!file.is_open())
+	{
+		ERROR("Failed to open file: " + path);
+		// return internal_server_error();
+		return;
+	}
+	file << request_packet.getContent();
+	file.close();
+	DEBUG("File uploaded to: " + path);
+	// return dummy_response();
+	(void)response_packet;
 }
